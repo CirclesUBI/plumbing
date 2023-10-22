c43f6d0bd49c19e729f5e375d3a5de0b7de1870b
ele <lluna@bitspossessed.org>
2023-10-22 21:48:45 +0200

    Add the analysis of the active wallets

--- garden-stats/index.js
+++ garden-stats/index.js
@@ -20,14 +20,23 @@ async function runScript() {
           },
     });
     await pool.connect();
+
+    // Deployed accounts are registered in the relayer
     const result = await pool.query('SELECT safe_id FROM relay_safecreation2 where tx_hash is not null');
     const rows = result.rows;
+
+    // Active accounts are the ones that have performed a transaction in the last 3 months
+    // data starts with "\xa2e62045" when an ubi issuance
+    const result2 = await pool.query(`select distinct safe_id from relay_safemultisigtx where modified > CURRENT_DATE - INTERVAL '3 months' and data not like '\xa2e62045';`);
+    const rows2 = result2.rows;
+
     pool.end()
 
     const web3 = new Web3(ETHEREUM_NODE_WS);
     // Get the Hub
     const hubContract = new web3.eth.Contract(Hub.abi, config.HUB_ADDR);
 
+    // Analyse the DEPLOYED wallets
     // Check that each safe is deployed
     let deployedIndividualWallets = 0;
     let deployedSharedWallets = 0;
@@ -45,6 +54,27 @@ async function runScript() {
     }));
     console.log(`Deployed individual wallets through garden: ${deployedIndividualWallets}`);
     console.log(`Deployed shared wallets through garden: ${deployedSharedWallets}`);
+
+
+    // Analyse the ACTIVE wallets
+    // Check that each safe is deployed
+    let activeIndividualWallets = 0;
+    let activeSharedWallets = 0;
+    console.log(`Rows: ${rows2.length}`);
+
+    await Promise.all(rows2.map(async (item) => {
+        if (await hubContract.methods.organizations(item.safe_id).call()) {
+            ++activeSharedWallets;
+        } else {
+            const tokenAddress = await hubContract.methods.userToToken(item.safe_id).call()
+            if (tokenAddress != ZERO_ADDRESS) {
+                ++activeIndividualWallets;
+            }
+        }
+    }));
+    console.log(`Avtive individual wallets in garden: ${activeIndividualWallets}`);
+    console.log(`Active shared wallets in garden: ${activeSharedWallets}`);
+
     process.exit(0);
 }
 
